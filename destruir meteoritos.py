a49da62d186ca82a511bd7d9e93de44ac5c3fdb1
lingwey <98229291+lingwey@users.noreply.github.com>
2023-07-26 17:19:21 -0300

    re acomoda los elementos y a la clase juego

--- destruir meteoritos.py
+++ destruir meteoritos.py
@@ -2,10 +2,6 @@ import pygame, random
 
 pantallaAlto = 680
 pantallaAncho = 720
-ventana = pygame.display.set_mode([pantallaAncho, pantallaAlto])
-reloj = pygame.time.Clock()
-imagenDeFondo = pygame.image.load("recursos/imagenes/background.png").convert()
-cerrar = False
 negro = (0,0,0)
 
 #agrega imagenes para el juego
@@ -19,7 +15,7 @@ class meteoro (pygame.sprite.Sprite):
     def update (self):
         #genera el moviemiento de caida de los meteoros
         self.rect.y += 1
-        if self.rect.y > pantallaAlto: #reinicia la caida de los meteoros cambiando la posicion del eje 3 
+        if self.rect.y > pantallaAlto: #reinicia la caida de los meteoros cambiando la posicion del eje x 
             self.rect.y = -10
             self.rect.x = random.randrange(pantallaAncho)
     
@@ -34,8 +30,8 @@ class player (pygame.sprite.Sprite):
     def update (self): #controla los movimeintos del jugador y mantiene limpio el codiog del bucle principal
         direcionMouse = pygame.mouse.get_pos()
          #print (direcionMouse)
-        jugador.rect.x = direcionMouse [0]
-        jugador.rect.y = direcionMouse [1]
+        self.rect.x = direcionMouse [0]
+        self.rect.y = direcionMouse [1]
 
 class laser (pygame.sprite.Sprite):
     def __init__ (self):
@@ -46,68 +42,79 @@ class laser (pygame.sprite.Sprite):
     
     def update (self): 
         self.rect.y -= 5
+
+#crea la clase juego
+class juego (object):
+    def __init__ (self, sonidoLaser, imagenDeFondo):
+        self.puntaje = 0
+        self.meteoroLista = pygame.sprite.Group()
+        self.totalListaImagenes = pygame.sprite.Group()
+        self.cantidadMeteoros = 50
+        self.lasersLista = pygame.sprite.Group()
         
         
-pygame.init()       
-#jugador = pygame.image.load("recursos/imagenes/player.png").convert()
-#jugador.set_colorkey([0,0,0,0])
-pygame.mouse.set_visible(0)
-meteoroLista = pygame.sprite.Group()
-lasersLista = pygame.sprite.Group()
-totalListaImagenes = pygame.sprite.Group()
-jugador =player()
-puntaje = 0
-totalListaImagenes.add(jugador)
-
-for i in range (50): #crea los meteoros y genera la posicion donde van a comienzar
-    meteoros = meteoro()
-    meteoros.rect.x = random.randrange(pantallaAncho)
-    meteoros.rect.y = random.randrange(pantallaAlto)
-    meteoroLista.add(meteoros)
-    totalListaImagenes.add(meteoros)
-    
-sonidoLaser = pygame.mixer.Sound("recursos/sonido/laser5.ogg")
-
-while not cerrar:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            cerrar = True
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            disparo = laser()
-            disparo.rect.x = jugador.rect.x + 45
-            disparo.rect.y = jugador.rect.y - 20
+        for i in range (self.cantidadMeteoros): #crea los meteoros y genera la posicion donde van a comienzar
+            meteoros = meteoro()
+            meteoros.rect.x = random.randrange(pantallaAncho)
+            meteoros.rect.y = random.randrange(pantallaAlto)
+            self.meteoroLista.add(meteoros)
+            self.totalListaImagenes.add(meteoros)
+            
+        self.jugador = player()
+        self.totalListaImagenes.add(self.jugador)
+        self.fondo = pygame.image.load(imagenDeFondo).convert()
+        self.sonido = sonidoLaser
+        
+    def process_events (self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+               return True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                disparo = laser()
+                disparo.rect.x = self.jugador.rect.x + 45
+                disparo.rect.y = self.jugador.rect.y - 20
            
             
-            totalListaImagenes.add(disparo)
-            lasersLista.add(disparo)
-            sonidoLaser.play()
-       
-    #el metodo update remplaza todo este bloque de codigo     
-    """direcionMouse = pygame.mouse.get_pos()
-    #print (direcionMouse)
-    jugador.rect.x = direcionMouse [0]
-    jugador.rect.y = direcionMouse [1] """
+                self.totalListaImagenes.add(disparo)
+                self.lasersLista.add(disparo)
+                self.sonido.play()
+        return False
+
+    def run_logic (self):
+        self.totalListaImagenes.update()
+        for disparo in self.lasersLista:
+            meteoroHitLista = pygame.sprite.spritecollide(disparo, self.meteoroLista, True)
+            for meteoro in meteoroHitLista:
+                self.totalListaImagenes.remove(disparo)
+                self.lasersLista.remove(disparo)
+                self.puntaje += 1
+                print (self.puntaje)
+            if disparo.rect.y < -10 :
+                self.totalListaImagenes.remove(disparo)
+                self.lasersLista.remove(disparo)
     
-    totalListaImagenes.update() #genera el movimiento a toda la lista de sprites 
-    
-    """meteoroHitLista = pygame.sprite.spritecollide(jugador, meteoroLista, True)
-    for meteor in meteoroHitLista:
-        puntaje += 1
-        print(puntaje)"""
-    for disparo in lasersLista:
-        meteoroHitLista = pygame.sprite.spritecollide(disparo, meteoroLista, True)
-        for meteoro in meteoroHitLista:
-            totalListaImagenes.remove(disparo)
-            lasersLista.remove(disparo)
-            puntaje += 1
-            print (puntaje)
-        if disparo.rect.y < -10 :
-            totalListaImagenes.remove(disparo)
-            lasersLista.remove(disparo)
-    #ventana.fill([255,255,255])
-    ventana.blit(imagenDeFondo,[0,0])
-    #ventana.blit(jugador, [x,y])
-    totalListaImagenes.draw(ventana)
-    pygame.display.flip()
-    reloj.tick(60)
-pygame.quit()
+    def display_frame (self,ventana):
+        ventana.blit(self.fondo,[0,0])
+        self.totalListaImagenes.draw(ventana)
+        pygame.display.flip()
+
+def main ():
+    pygame.init() 
+    imagenDeFondo = "recursos/imagenes/background.png"
+    sonidoLaser = pygame.mixer.Sound("recursos/sonido/laser5.ogg")
+    ventana = pygame.display.set_mode([pantallaAncho, pantallaAlto])
+    cerrar = False
+    reloj = pygame.time.Clock()
+    game = juego( sonidoLaser, imagenDeFondo)
+    pygame.mouse.set_visible(0)
+   
+    while not cerrar:
+        cerrar = game.process_events()
+        game.run_logic()
+        game.display_frame(ventana)
+        reloj.tick(60)
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()      
+
